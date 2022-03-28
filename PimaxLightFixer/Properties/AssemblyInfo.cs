b7d63cc23e84ee2cc94fe1e7c36b1e4f0394a004
guppyexpress <guppyexpress@gmail.com>
2022-03-27 19:45:53 -0500

    fixed versioning

--- PimaxLightFixer/Properties/AssemblyInfo.cs
+++ PimaxLightFixer/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.31.2")]
-[assembly: AssemblyFileVersion("2.31.2")]
+[assembly: AssemblyVersion("2.31.3")]
+[assembly: AssemblyFileVersion("2.31.3")]
